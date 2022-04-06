150e5304454d4d6d6e62ace91b2ea87d925b3415
shujanaqvi12 <wahaj687@gmail.com>
2022-04-06 20:17:57 +0500

    333

--- index.js
+++ index.js
@@ -4,8 +4,23 @@
  */
 require("dotenv").config();
 const app = require("express")();
-const port =8080;
+const port = 8080;
 require("./database");
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+
+  next();
+});
 
 // Middleware
 require("./middleware/common")(app);
